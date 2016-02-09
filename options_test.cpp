70a31d30faf0f8da5db4eed437bf2efb7432b346
Ola Thoresen <ola@nytt.no>
2016-02-09 10:54:53 +0100

    Cleanup and add comments

--- options_test.cpp
+++ options_test.cpp
@@ -10,14 +10,29 @@ using namespace std;
 // using namespace isc::dhcp;
 
 
-int replaceOption(int option_number, int subopt_number, string option_data, string placeholder, string replace_with) 
+int o2o_replaceOption(int option_number, int subopt_number, string option_data, string placeholder, string replace_with) 
 {
-	
 	cout << option_number << "." << subopt_number << " - " << option_data << " -> ";
 	option_data.replace(option_data.find(placeholder), placeholder.size(), replace_with);
 	cout << "option-data2: " << option_data << "\n";
-
 }
+
+string o2o_getOption(int option_number, int subopt_number, int sanitize = 1) {
+	string value;
+	// Get option
+	if (subopt_number > 0) {
+
+	}
+	else {
+
+	}
+	if (sanitize > 0) {
+		// Sanitize it
+	}
+	// Return it	
+	return value;
+}
+
 int main()
 {
 	// Test
@@ -43,77 +58,64 @@ int main()
 
 	// Options we might write to
 	// These are the options that will be scanned for "variables" or "placeholders". Just add all _potential_ options here, as nothing is done if the placceholder is not present.
-	// Also notice that options without sub options will use sub option id = 0.  You can use the pre defined option names 
+	// You can use the pre defined option names 
+	// Also notice that options without sub options will use sub option id = 0.  
 	options_out[43][1] = 1;
 	options_out[43][2] = 1;
 	options_out[DHO_BOOT_FILE_NAME][0] = 1;
 
+	// Get the option-values from the dhcp-request
 	for ( const auto &opt_i : options_in ) {
-		// string opt_i_match;
 		for ( const auto &sub_i : options_in[opt_i.first]) {
-			// opt_i_match = PRE_POST_FIX + "OPTION_" + to_string(opt_i.first);
 			if (sub_i.first > 0) {
-				// opt_i_match += "_" + to_string(sub_i.first);
 				options_variables["OPTION_" + to_string(opt_i.first) + "_" + to_string(sub_i.first)] = "foo" + to_string(opt_i.first) + "." + to_string(sub_i.first); // getOption();
 			}
 			else {
 				options_variables["OPTION_" + to_string(opt_i.first)] = "bar" + to_string(opt_i.first); // getOption();
 			}
-			// opt_i_match += PRE_POST_FIX;
 		}
 	}
+
+	// Also get some other variables
 	options_variables["HWADDR"] = "hwaddr";
 	options_variables["IPADDR"] = "ipaddr";
+
+	// ... and generate a few variants that might be useful
 	options_variables["HWADDR_CISCO"] = "hwaddr_cisco";
+	options_variables["IPADDR_HEX"] = "ipaddr_hex";
+
 
 	// Then we search the out-options for the identificator
 	for ( const auto &opt_o : options_out ) {
-		string opt_o_match;
 		for ( const auto &sub_o : options_out[opt_o.first]) {
-			opt_o_match = "option-" + to_string(opt_o.first);
 			string option_data;
 			int option_number = opt_o.first;
 			int subopt_number = 0;
 			if (sub_o.first > 0) {
 				// string option_data = getOption(sub)
+				// This must NOT be sanitized, as it might contain the placeholder-identificator which is supposed to be replaced...
 				option_data = "test.@OPTION_82_" + to_string(sub_o.first) + "@.conf";
 				subopt_number = sub_o.first;
 				cout << option_number << "." << subopt_number << "-option-data: " << option_data << "\n";
 			}
 			else {
 				// string option_data = getOption(opt)
+				// This must NOT be sanitized, as it might contain the placeholder-identificator which is supposed to be replaced...
 				option_data = "test.@HWADDR@.conf";
 				cout << option_number << "-option-data: " << option_data << "\n";
 			}
 			for ( const auto &ov : options_variables) {
-				// cout << "Key: " << ov.first << " Val: " << ov.second << "\n";
-				// regex var;
-				// regcomp  ov.first;
 				string var = PRE_POST_FIX + ov.first + PRE_POST_FIX;
-				// cout << "Var: " << var  << "\n";
-				// cout << "Len: " << var.size() << "\n";
 				string res = ov.second;
-       				// option_data = regex_replace (option_data, var, res);
-       				// option_data.at("HWADDR") = res;
-       				if (option_data.find(var) != std::string::npos) {
-					replaceOption(option_number, subopt_number, option_data, var, res);
-					// option_data.replace(option_data.find(var), var.size(), res);
-					// cout << "option-data2: " << option_data << "\n";
-				}
-				// else {
-				// 	cout << "Could not find " << ov.first << " in " << option_data << "\n";
-				// }
-				// s.replace(s.find("$name"), sizeof("Somename")-1, "Somename");
 
+       				if (option_data.find(var) != std::string::npos) {
+					// If the placeholder is present in the option-data, we replace it with the correct value
+					o2o_replaceOption(option_number, subopt_number, option_data, var, res);
+					// Then we write the option back to the dhcp-packet
+				}
 			}
-			// if match opt_i_match out.toString() -> setOption()
-			// Code here...!
-			// opt_o_match += "-" + to_string(sub_o.first);
-			// }
-			// cout << "in: " << opt_i_match << " out: " << opt_o_match << "\n";
 		}
 	}
-
 }
 
 
